88afd985d8d203e842c64053e549574f76f273f9
Cheikh Tacko SECK <cheikhseck24@gmail.com>
2022-04-11 01:59:54 +0000

    Initial Commit

---
+++ MyMovieDB/MyMovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMovieDBApp.swift
+//  MyMovieDB
+//
+//  Created by Cheikh SECK on 11/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyMovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
